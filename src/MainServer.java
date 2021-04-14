cd97ddc1bfeceeb1c8185c1a97e1e9f06ad0391f
Maciej Mlaskawa <maciej.mlaskawa@gmail.com>
2021-04-14 14:56:37 +0200

    Implemented basic method accepting clients

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,2 +1,30 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
+
+    static final int port = 2020;
+
+    public void startAcceptingClients(){
+        ServerSocket serverSocket;
+        Socket newClient;
+
+        try{
+            serverSocket = new ServerSocket(port);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return;
+        }
+
+        while(true){
+            try {
+                newClient = serverSocket.accept();
+                new NumbersAdder(newClient).run();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
